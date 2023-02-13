e2652c96a2833e85012d369a5e8c92e4ca9db0d2
Eduardo73Martinez <martin61martinez@hotmail.com.ar>
2023-02-12 23:36:37 -0300

    acomode los entry y labels y le cambie el estilo de letras

--- GenerarContraseña.py
+++ GenerarContraseña.py
@@ -6,7 +6,7 @@ raiz= Tk()
 raiz.geometry("450x420")
 raiz.resizable(0,0)
 raiz.title("Generador de contraseñas")
-raiz['bg'] = '#49A'
+raiz['bg'] = '#83aeab'
 
 capturaSimbolos= StringVar()
 tamañoContraseña = StringVar()
@@ -58,28 +58,30 @@ def añadirNumerosSiHayIndicacion():
 
 #los labels 
 
-cuadroTamaño = Label ( raiz, text="Tamaño contraseña:  ").place(x=10, y=50) 
+cuadroTamaño = Label ( raiz, text="Cantidad \n caracteres:  ").place(x=10, y=70) 
 #cuadroTamaño.grid( row = 0, column = 0 , pady = 30 , padx = 10)
 
 
-cuadroSimbolos = Label ( raiz, text="Simbolos a usar \n (opcional):  ").place(x=10, y=115) 
+cuadroSimbolos = Label ( raiz, text="Simbolos a usar \n (opcional):  ").place(x=10, y=125) 
 #cuadroSimbolos.grid( row = 1, column = 0 , sticky ="" , pady = 30 , padx = 10)
 
 
 
 
-milabel=Label(raiz, text="Especifica los campos", fg="red" ,bg="white", font=("Comic Sans MS", 15),bd="2px").place(x=130, y=5)
+milabel=Label(raiz, text="Especifica los campos", fg="red" ,bg="white", font=("Arial 18", 15),bd="2px").place(x=130, y=5)
 
 #los entry
-cuadroEntradaTamaño=Entry(raiz, textvariable=tamañoContraseña, width= 10, font='Arial 15', fg='Grey').place(x=170, y=50) 
-cuadroEntradaSimbolos=Entry(raiz, textvariable= capturaSimbolos, width= 20, font='Arial 14', fg='Grey').place(x=130, y=115) 
-cuadroRespuesta=Entry(raiz, textvariable= contraseñaGenerada, width= 30, font='Arial 18', fg='Grey').place(x=24, y=340) 
+cuadroEntradaTamaño=Entry(raiz, textvariable=tamañoContraseña, width= 10, font='Arial 15', fg='Grey').place(x=130, y=70) 
+
+cuadroEntradaSimbolos=Entry(raiz, textvariable= capturaSimbolos, width= 25, font='Arial 15', fg='Grey').place(x=130, y=135) 
+cuadroRespuesta=Entry(raiz, textvariable= contraseñaGenerada, width= 30, font='Arial 18', fg='Grey').place(x=24, y=330) 
 
 
 #botones
 botonGenerar=Button(raiz, text="Generar contraseña", command= lambda:mostrarContraseña()).place(x=160, y=380) 
 
-C1 = Checkbutton(raiz, text = "Usar mayusculas", variable= hayMayusculas, onvalue = 1, offvalue = 0, height=0, width = 0, font='Arial 12').place(x=150, y=210) 
-C2 = Checkbutton(raiz, text = " Usar números   ", variable= hayEnteros, onvalue = 1, offvalue = 0, height=0, width = 0, font='Arial 12').place(x=150, y=240)
+
+Check1 = Checkbutton(raiz, text = "Usar mayusculas", variable= hayMayusculas, onvalue = 1, offvalue = 0, height=0, width = 0, font='Arial 12', fg="red").place(x=130, y=210) 
+Check2 = Checkbutton(raiz, text = " Usar números   ", variable= hayEnteros, onvalue = 1, offvalue = 0, height=0, width = 0, font='Arial 12', fg="red").place(x=130, y=280)
 
 raiz.mainloop()
